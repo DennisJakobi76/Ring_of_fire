b83df19e3ddf3da5f1981aafc63edbba3edd7635
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-03-10 16:49:59 +0100

    added card stack to game html

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,11 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-game',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  styleUrl: './game.component.scss',
 })
-export class GameComponent {
-
-}
+export class GameComponent {}
