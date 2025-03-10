b4b732f5962cae5e5e54ddf86465833eb7b1ef27
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-03-10 16:25:33 +0100

    added game component

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-start-screen',
   imports: [],
   templateUrl: './start-screen.component.html',
-  styleUrl: './start-screen.component.scss'
+  styleUrl: './start-screen.component.scss',
 })
 export class StartScreenComponent {
-
+  constructor(private router: Router) {}
+  newGame() {
+    // Start game
+    this.router.navigate(['game']);
+  }
 }
