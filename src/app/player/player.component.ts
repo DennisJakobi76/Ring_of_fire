8ae6a0be6a632e979b8889191cd83e2850e4bd12
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-03-11 12:04:05 +0100

    bound player name to game component

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-player',
   imports: [],
   templateUrl: './player.component.html',
-  styleUrl: './player.component.scss'
+  styleUrl: './player.component.scss',
 })
 export class PlayerComponent {
+  @Input() name?: string;
 
+  constructor() {}
 }
