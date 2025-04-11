6cc6e9ffb2d273c38b43b99371c59ed45e865f95
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-04-11 14:18:29 +0200

    improved key-handling

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { environment } from '../environments/environment';
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
@@ -14,7 +14,7 @@ export const appConfig: ApplicationConfig = {
         projectId: 'ring-of-fire-15c98',
         appId: '1:139234515109:web:5a78c0fe67df2837a7954c',
         storageBucket: 'ring-of-fire-15c98.firebasestorage.app',
-        apiKey: 'AIzaSyAfYtB09_s-rlepT00m3BFDfxrH33fWTQk',
+        apiKey: environment.apiKey,
         authDomain: 'ring-of-fire-15c98.firebaseapp.com',
         messagingSenderId: '139234515109',
       })
